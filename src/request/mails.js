797fb8b916da2633ae5e1608536e30e26ebc03f7
ginozhu <gino_zhu@epam.com>
2018-06-24 00:10:00 +0800

    update data struc

--- src/request/mails.js
+++ src/request/mails.js
@@ -53,7 +53,7 @@ async function getParsedMessages () {
         messages: []
       })
     }
-    result[message.floor][message.unit].messages.push(
+    result[message.floor][message.unit].messages.unshift(
       {
         id: message._id,
         content: message.content,
